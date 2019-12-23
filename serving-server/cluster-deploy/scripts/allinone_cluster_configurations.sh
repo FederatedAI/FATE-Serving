e9b890774dc2e906efce95094f816012dd518494
qianduoduo <1002502212@qq.com>
2019-12-23 15:24:47 +0800

    serving 部署脚本

--- serving-server/cluster-deploy/scripts/allinone_cluster_configurations.sh
+++ serving-server/cluster-deploy/scripts/allinone_cluster_configurations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 user=app
-host_guest=(127.0.0.1 127.0.0.1)
-roll_hostAndguest=(127.0.0.1 127.0.0.1)
+host_guest=(192.168.0.1 192.168.0.2)
+roll_hostAndguest=(192.168.0.1 192.168.0.2)
 deploy_dir=/data/projects
 redis_password=fate_dev
 apply_zk=true
