80dd8d02cf4c7a01ccf2d3501b4e0442691d575a
qianduoduo <yu1002502212yu>
2019-12-31 17:59:42 +0800

    shell auto cluster project

--- serving-server/cluster-deploy/scripts/package.sh
+++ serving-server/cluster-deploy/scripts/package.sh
@@ -17,47 +17,11 @@ fate_serving_zip=fate-serving-server-*-release.zip
 fate_serving_jar=fate-serving-server-*.jar
 router_zip=fate-serving-router-*-release.zip
 router_jar=fate-serving-router-*.jar
-common=$public_path/fate-serving/common
 router=router
 router_path=$fate_serving_path/router
 
-if [ ! -d $common  ]; then
-	mkdir -p $common
-else
-  echo [INFO] $common dir exist
-fi
-cp jdk_install.sh $common
-cp redis_install.sh $common
-cp services.sh $fate_serving_path
-
-cd $common
-echo "[INFO] jdk install"
-#sudo sh jdk_install.sh
-#sudo rm -rf jdk_install.sh
-echo "[INFO] redis install"
-sudo sh redis_install.sh
-sudo rm -rf redis_install.sh
-cd redis/conf
-sudo sed -i.bak "s/# requirepass foobared/requirepass ${redis_password}/g" ./redis.conf
-sudo sed -i.bak "s/databases 16/databases 50/g" ./redis.conf
-
-cd ${cwd}/../../../
-echo "[INFO] mvn clean package  start"
-mvn clean package -DskipTests
-if [ $? -eq 0 ]; then
-	echo  "[INFO]  mvn clean package success"
-else
-	echo "[INFO]  mvn clan package filed"
-fi
-
 if [ ! -d $fate_serving_path  ]; then
-	cd $public_path
-	mkdir $fate_serving
-else
-  echo [INFO] $fate_serving_path dir exist
-fi
-
-if [ -d $fate_serving_path  ]; then
+	mkdir -p $fate_serving_path
 	if [ ! -d $sering_path  ]; then
 		cd $fate_serving_path
 		mkdir $serving
@@ -65,57 +29,67 @@ if [ -d $fate_serving_path  ]; then
 	else
 		echo [INFO] $fate_serving_path dir exist
 	fi
-	
-else
-  echo ""
 fi
+cp services.sh $fate_serving_path
+cd ${cwd}/../../../
+echo "[INFO] mvn clean package  start"
+mvn clean package -DskipTests
+if [ $? -eq 0 ]; then
+	echo  "[INFO]  mvn clean package success"
+else
+	echo "[INFO]  mvn clan package filed"
+	exit
+fi
+
 
 cd $cwd/../../target
 cp $fate_serving_zip $sering_path
-if [ $? -eq 0 ]; then
-	echo  "[INFO]  cp fate-serving-server-*-release.zip success"
-else
-	echo "[INFO]  cp fate-serving-server-*-release.zip filed"
-fi
 cd $sering_path
 unzip $fate_serving_zip
 ln -s $fate_serving_jar  fate-serving-server.jar
-
-echo "[INFO]  cp fate-serving-router-*-release.zip success"
 cd $cwd/../../../router/target
 cp $router_zip $router_path
-if [ $? -eq 0 ]; then
-	echo  "[INFO]  cp fate-serving-router-*-release.zip success"
-else
-	echo "[INFO]  cp fate-serving-server-*-release.zip filed"
-fi
-echo "-------------------routerpach"
 cd $router_path
 unzip $router_zip
 ln -s $router_jar fate-serving-router.jar
 echo "--------------------ln -s"
+cd  $sering_path/conf
+sed -i 's#redis.ip=127.0.0.1#'redis.ip=${host_redis_ip}'#' serving-server.properties
+sed -i 's#redis.port=6379#'redis.port=${host_redis_port}'#' serving-server.properties
+sed -i 's#redis.password=fate_dev#'redis.password=${host_redis_password}'#' serving-server.properties
+sed -i "/^workMode=/cworkMode=${workMode}" serving-server.properties
+echo "------------------------- sed"
 cd $cwd
-cp  start_env/services.sh $public_path/fate-serving/common
-echo "------------apply----" ${apply_zk}
-
+echo "-------------------- cwd" $cwd
 if [ ${apply_zk} = "false" ]
 then
+	echo "---------- apply_zk false"
 	cd $router_path/conf
 	sed -i 's#useRegister=true#'useRegister=false'#' proxy.properties
 	sed -i 's#useZkRouter=true#'useZkRouter=false'#' proxy.properties
 	cd  $sering_path/conf
 	sed -i 's#useRegister=true#'useRegister=false'#' serving-server.properties
 	sed -i 's#useZkRouter=true#'useZkRouter=false'#' serving-server.properties
+elif [ ${apply_zk} = "true" ]
+then
+	echo "---------apply _zk true"
+	cd $router_path/conf
+	sleep 6
+	echo "----------------" ${host_zk_url}
+	echo "----------------" ${host_zk_url}
+	echo "----------------" ${host_zk_url}
+	echo "----------------" ${host_zk_url}
+	echo "----------------" ${host_zk_url}
+	echo "----------------" ${host_zk_url}
+	echo "----------------" pwd
+	echo "----------------" $pwd
+	sed -i "/^zk.url=/czk.url=${host_zk_url}" proxy.properties
+	cd  $sering_path/conf
+	sed -i 's#zk.url=zookeeper://localhost:2181#'zk.url=${host_zk_url}'#' serving-server.properties
+else 
+		echo ""
 fi
-	sh zookeeper_install.sh
-	cd $cwd
-	echo "-------zkui_instal start"
-	sh zkui_install.sh
-
-sudo cp redis/service.sh  $common/redis
-cp zk/service.sh  $common/zookeeper*
-cp zkui/service.sh  $common/zkui
-echo "---------------------sed roll"
+echo "------------- zk uil"
 if [[ ${host_guest[0]} ]]
 then
 	cd ${public_path}/fate-serving/serving/conf
@@ -135,8 +109,8 @@ init_env() {
 eeooff
 		cd ${public_path}
 		tar -zcvf fate-serving.tar.gz  fate-serving/
-
-		scp ${public_path}/fate-serving.tar.gz @${host_guest[1]}:${public_path}
+		echo "-----------------scp-:"  ${host_guest[1]}
+		scp ${public_path}/fate-serving.tar.gz ${host_guest[1]}:${public_path}
         ssh -tt ${user}@${host_guest[1]} << eeooff
 	cd ${public_path}
 	echo "public_path-------" ${public_path}
@@ -144,6 +118,12 @@ eeooff
 	rm -rf fate-serving.tar.gz
 	cd ${public_path}/fate-serving/serving/conf
 	sed -i 's#${roll_hostAndguest[0]}#'${roll_hostAndguest[1]}'#' serving-server.properties
+	sed -i 's#zk.url=${host_zk_url}#'zk.url=${guest_zk_url}'#' serving-server.properties
+	sed -i 's#redis.ip=${host_redis_ip}#'redis.ip=${guest_redis_ip}'#' serving-server.properties
+	sed -i 's#redis.port=${host_redis_port}#'redis.port=${guest_redis_port}'#' serving-server.properties
+	sed -i 's#redis.password=${host_redis_password}#'redis.password=${guest_redis_password}'#' serving-server.properties
+	 cd ${router_path}/conf
+       	sed -i '/^zk.url=/czk.url=${guest_zk_url}' proxy.properties
 exit
 eeooff
 
