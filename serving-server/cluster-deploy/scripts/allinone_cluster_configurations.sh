61e410f79fc09ed13a36d13d4137bc817e5a982e
qianduoduo <yu1002502212yu>
2020-01-03 12:02:37 +0800

    shell ip problem

--- serving-server/cluster-deploy/scripts/allinone_cluster_configurations.sh
+++ serving-server/cluster-deploy/scripts/allinone_cluster_configurations.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 user=app
-host_guest=(127.0.0.1 127.0.0.1)
-roll_hostAndguest=(127.0.0.1 127.0.0.1)
+host_guest=(192.168.0.1 192.168.0.2)
+roll_hostAndguest=(192.168.0.1 192.168.0.2)
 deploy_dir=/data/projects
 host_redis_ip=127.0.0.1
-host_redis_port=63792
+host_redis_port=6379
 host_redis_password=fate_dev
 guest_redis_ip=127.0.0.1
 guest_redis_port=6379
