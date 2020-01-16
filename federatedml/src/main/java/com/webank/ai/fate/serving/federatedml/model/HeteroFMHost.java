ca9d0dea6c535a9cd918b280ca8b20ab3a750b0a
lindazheng(郑琳琳) <lindazheng@webank.com>
2020-01-16 20:19:55 +0800

    add fm online predict

---
+++ federatedml/src/main/java/com/webank/ai/fate/serving/federatedml/model/HeteroFMHost.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 The FATE Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.webank.ai.fate.serving.federatedml.model;
+
+import com.webank.ai.fate.serving.core.bean.Context;
+import com.webank.ai.fate.serving.core.bean.Dict;
+import com.webank.ai.fate.serving.core.bean.FederatedParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HeteroFMHost extends HeteroFM {
+    private static final Logger logger = LoggerFactory.getLogger(HeteroFMHost.class);
+
+    @Override
+    public Map<String, Object> handlePredict(Context context, List<Map<String, Object>> inputData, FederatedParams predictParams) {
+
+        logger.info("hetero fm host begin to predict");
+        HashMap<String, Object> result = new HashMap<>();
+        Map<String, Object> ret = forward(inputData);
+        result.put(Dict.SCORE, ret.get(Dict.SCORE));
+        result.put(Dict.FM_CROSS, ret.get(Dict.FM_CROSS));
+
+        logger.info("hetero fm host predict ends, result is {}", result);
+
+        return result;
+    }
+}
