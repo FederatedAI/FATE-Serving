cb9faad372ca8936dcab193d572164d062e3f1c9
kaideng <forgive_dengkai@163.com>
2020-01-21 11:40:31 +0800

    change struct
    Signed-off-by: kaideng <forgive_dengkai@163.com>

--- fate-serving-core/src/main/java/com/webank/ai/fate/serving/core/bean/Dict.java
+++ fate-serving-core/src/main/java/com/webank/ai/fate/serving/core/bean/Dict.java
@@ -157,4 +157,23 @@ public class Dict {
     public  static  final String MD5_SALT = "$1$ML";
 
 
+    public static final String CASE_ID="caseid";
+    public static final String  CODE ="code";
+    public static final String  MESSAGE ="message";
+    public static final String MODEL_ID = "modelId";
+    public static final String MODEL_VERSION = "modelVersion";
+    public static final String APP_ID = "appid";
+    public static final String PARTY_ID = "partyId";
+    public static final  String  FEATURE_DATA = "featureData";
+
+    public static final String DEFAULT_VERSION = "1.0";
+    public static final String SELF_PROJECT_NAME = "proxy";
+    public static final String SELF_ENVIRONMENT = "online";
+    public static final String HEAD = "head";
+    public static final String BODY = "body";
+    public static final String SERVICENAME_INFERENCE = "inference";
+    public static final String SERVICENAME_START_INFERENCE_JOB = "startInferenceJob";
+    public static final String SERVICENAME_GET_INFERENCE_RESULT = "getInferenceResult";
+
+
 }
