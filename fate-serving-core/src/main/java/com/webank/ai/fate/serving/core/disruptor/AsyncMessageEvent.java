c9cb273b52ec573f37f48642324a87929ec24434
v_dylanxu <136539068@qq.com>
2020-02-26 09:27:36 +0800

    ok
    Signed-off-by: v_dylanxu <136539068@qq.com>

--- fate-serving-core/src/main/java/com/webank/ai/fate/serving/core/disruptor/AsyncMessageEvent.java
+++ fate-serving-core/src/main/java/com/webank/ai/fate/serving/core/disruptor/AsyncMessageEvent.java
@@ -1,8 +1,9 @@
 package com.webank.ai.fate.serving.core.disruptor;
 
-import com.alibaba.fastjson.JSONObject;
 import com.lmax.disruptor.EventFactory;
+import lombok.Data;
 
+@Data
 public class AsyncMessageEvent<T> {
 
     private String name;
@@ -13,52 +14,10 @@ public class AsyncMessageEvent<T> {
 
     private T data;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setData(T data) {
-        this.data = data;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public AsyncMessageEvent() {
-    }
-
     void clear() {
         data = null;
     }
 
     public static final EventFactory<AsyncMessageEvent> FACTORY = () -> new AsyncMessageEvent();
 
-    @Override
-    public String toString() {
-        return "AsyncMessageEvent{" +
-                "name='" + name + '\'' +
-                ", data=" + JSONObject.toJSONString(data) +
-                '}';
-    }
 }
