f2b59ccd029d24711623c66bdb25709836c84070
kaideng <forgive_dengkai@163.com>
2020-04-16 14:38:15 +0800

    fix common pre process
    Signed-off-by: kaideng <forgive_dengkai@163.com>

--- serving-server/src/main/java/com/webank/ai/fate/serving/adapter/processing/CommonPreProcessing.java
+++ serving-server/src/main/java/com/webank/ai/fate/serving/adapter/processing/CommonPreProcessing.java
@@ -1,6 +1,7 @@
 package com.webank.ai.fate.serving.adapter.processing;
 
 
+import com.alibaba.fastjson.JSON;
 import com.webank.ai.fate.serving.bean.PreProcessingResult;
 import com.webank.ai.fate.serving.core.bean.Context;
 import jdk.nashorn.internal.runtime.ParserException;
@@ -10,22 +11,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CommonPreProcessing implements PreProcessing {
+
     @Override
     public PreProcessingResult getResult(Context context , String paras) {
         PreProcessingResult preProcessingResult = new PreProcessingResult();
-        preProcessingResult.setProcessingResult(preProcessing(paras));
-        Map<String, Object> featureIds = new HashMap<>();
-        JSONObject paraObj = new JSONObject(paras);
-        preProcessingResult.setFeatureIds(featureIds);
+        preProcessingResult.setProcessingResult( JSON.parseObject(paras, HashMap.class));
+        preProcessingResult.setFeatureIds(preProcessingResult.getProcessingResult());
         return preProcessingResult;
     }
 
-    private Map<String, Object> preProcessing(String paras) throws ClassCastException, ParserException {
-        Map<String, Object> feature = new HashMap<>();
-
-        return feature;
-    }
-
-    public static  void main(String[] args){
-    }
 }
