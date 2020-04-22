4f1e5b5abf06bbfa2e582be4c4bb792256dd038e
tesla7 <77625961@qq.com>
2020-04-22 18:36:07 +0800

    add:docker build update:pack version to 1.2.4

---
+++ docker-build/build_cluster_docker.sh
@@ -0,0 +1,60 @@
+########################################################
+# Copyright 2019-2020 program was created VMware, Inc. #
+# SPDX-License-Identifier: Apache-2.0                  #
+########################################################
+
+#!/usr/bin/env bash
+set -e
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+WORKINGDIR=`pwd`
+
+source_code_dir=$(cd `dirname ${WORKINGDIR}`; pwd)
+
+
+package() {
+  docker run --rm -u $(id -u):$(id -g) -v ${source_code_dir}:/data/projects/fate/FATE-Serving --entrypoint="" maven:3.6-jdk-8 /bin/bash -c "cd /data/projects/fate/FATE-Serving && mvn clean package -DskipTests"
+}
+
+buildModule() {
+  source .env
+  for module in "serving-proxy" "serving-server"
+  do
+      echo "### START BUILDING ${module} ###"
+      docker build --build-arg version=${version} --build-arg PREFIX=${PREFIX} -t ${PREFIX}/${module}:${TAG} -f ${source_code_dir}/docker-build/${module}/Dockerfile ${source_code_dir}
+      echo "### FINISH BUILDING ${module} ###"
+      echo ""
+  done;
+}
+
+pushImage() {
+  ## push image
+  source .env
+  for module in "serving-proxy" "serving-server"
+  do
+      echo "### START PUSH ${module} ###"
+      docker push ${PREFIX}/${module}:${TAG}
+      echo "### FINISH PUSH ${module} ###"
+      echo ""
+  done;
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+         modules)
+                 buildModule
+                 ;;
+         package)
+                 package
+                 ;;
+         all)
+                 package
+                 buildModule
+                 ;;
+         push)
+                pushImage
+                ;;
+    esac
+    shift
+done
