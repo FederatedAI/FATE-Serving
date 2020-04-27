ec869b83430c762408e252a93dfca3f0e14c8e92
kaideng <forgive_dengkai@163.com>
2020-04-27 09:47:27 +0800

    ok

--- serving-server/src/main/java/com/webank/ai/fate/serving/host/provider/HostBatchInferenceProvider.java
+++ serving-server/src/main/java/com/webank/ai/fate/serving/host/provider/HostBatchInferenceProvider.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
         "requestOverloadBreaker",
         "hostBatchParamInterceptor",
         "hostModelInterceptor",
-        "batchFeatureAdaptorInterceptor"
+        "hostBatchFeatureAdaptorInterceptor"
 }, postChain = {
         "monitorInterceptor"
 })
