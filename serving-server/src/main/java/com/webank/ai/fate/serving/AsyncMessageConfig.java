b30685f2385ff66806ad395456ae4f51af618c9a
v_dylanxu <136539068@qq.com>
2020-05-09 11:04:49 +0800

    ok
    Signed-off-by: v_dylanxu <136539068@qq.com>

--- serving-server/src/main/java/com/webank/ai/fate/serving/AsyncMessageConfig.java
+++ serving-server/src/main/java/com/webank/ai/fate/serving/AsyncMessageConfig.java
@@ -1,14 +1,26 @@
 package com.webank.ai.fate.serving;
 
+import com.webank.ai.fate.serving.core.annotation.Subscribe;
+import com.webank.ai.fate.serving.core.async.AbstractAsyncMessageProcessor;
 import com.webank.ai.fate.serving.core.async.AsyncSubscribeRegister;
 import com.webank.ai.fate.serving.core.async.DefaultAsyncMessageProcessor;
 import com.webank.ai.fate.serving.core.bean.SpringContextUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
 @Configuration
 public class AsyncMessageConfig {
+
+    Logger logger = LoggerFactory.getLogger(AsyncMessageConfig.class);
 
     @Bean
     @ConditionalOnMissingBean
@@ -17,13 +29,39 @@ public class AsyncMessageConfig {
     }
 
     @Bean
-    public AsyncSubscribeRegister asyncSubscribeRegister() {
-        return new AsyncSubscribeRegister();
+    public ApplicationListener<ApplicationReadyEvent> asyncSubscribeRegister() {
+
+        return new ApplicationListener<ApplicationReadyEvent>() {
+            @Override
+            public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+                String[] beans = SpringContextUtil.getBeanNamesForType(AbstractAsyncMessageProcessor.class);
+                for (String beanName : beans) {
+                    AbstractAsyncMessageProcessor eventProcessor =  SpringContextUtil.getBean(beanName,AbstractAsyncMessageProcessor.class);
+                    Method[] methods = eventProcessor.getClass().getMethods();
+                    for (Method method : methods) {
+                        if (method.isAnnotationPresent(Subscribe.class)) {
+                            Subscribe subscribe = method.getAnnotation(Subscribe.class);
+                            if (subscribe != null) {
+                                Set<Method> methodList = AsyncSubscribeRegister.SUBSCRIBE_METHOD_MAP.get(subscribe.value());
+                                if (methodList == null) {
+                                    methodList = new HashSet<>();
+                                }
+                                methodList.add(method);
+                                AsyncSubscribeRegister.METHOD_INSTANCE_MAP.put(method,eventProcessor);
+                                AsyncSubscribeRegister.SUBSCRIBE_METHOD_MAP.put(subscribe.value(), methodList);
+                            }
+                        }
+                    }
+                }
+
+                logger.info("subscribe register info {}", AsyncSubscribeRegister.SUBSCRIBE_METHOD_MAP.keySet());
+            }
+        };
     }
 
-    @Bean
+    /*@Bean
     public DefaultAsyncMessageProcessor defaultAsyncMessageProcessor() {
         return new DefaultAsyncMessageProcessor();
-    }
+    }*/
 
 }
