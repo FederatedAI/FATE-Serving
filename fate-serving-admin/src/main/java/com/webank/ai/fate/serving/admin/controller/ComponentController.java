8784ad2be4eb5ee74ef34d64767326e1b416d3a7
v_dylanxu <136539068@qq.com>
2020-05-19 14:36:31 +0800

    add component basic info
    Signed-off-by: v_dylanxu <136539068@qq.com>

--- fate-serving-admin/src/main/java/com/webank/ai/fate/serving/admin/controller/ComponentController.java
+++ fate-serving-admin/src/main/java/com/webank/ai/fate/serving/admin/controller/ComponentController.java
@@ -1,20 +1,28 @@
 package com.webank.ai.fate.serving.admin.controller;
 
-import com.google.common.collect.Lists;
-import com.webank.ai.fate.register.url.CollectionUtils;
-import com.webank.ai.fate.register.zookeeper.ZookeeperClient;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
+import com.webank.ai.fate.api.networking.common.CommonServiceGrpc;
+import com.webank.ai.fate.api.networking.common.CommonServiceProto;
 import com.webank.ai.fate.register.zookeeper.ZookeeperRegistry;
 import com.webank.ai.fate.serving.admin.services.ComponentService;
+import com.webank.ai.fate.serving.core.bean.GrpcConnectionPool;
 import com.webank.ai.fate.serving.core.bean.ReturnResult;
+import io.grpc.ManagedChannel;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * @Description Service management
@@ -30,15 +38,48 @@ public class ComponentController {
     @Autowired
     ZookeeperRegistry  zookeeperRegistry;
 
-
     @Autowired
     ComponentService  componentServices;
 
+    @Value("${grpc.timeout:5000}")
+    private int timeout;
 
+    GrpcConnectionPool grpcConnectionPool = GrpcConnectionPool.getPool();
 
     @GetMapping("/component/list")
     public ComponentService.NodeData list() {
       return  componentServices.getCachedNodeData();
+    }
+
+    @GetMapping("/component/listProps")
+    public ReturnResult listProps(String host, int port, String keyword) {
+        ManagedChannel managedChannel = grpcConnectionPool.getManagedChannel(host, port);
+        CommonServiceGrpc.CommonServiceBlockingStub blockingStub = CommonServiceGrpc.newBlockingStub(managedChannel);
+        blockingStub = blockingStub.withDeadlineAfter(timeout, TimeUnit.MILLISECONDS);
+
+        CommonServiceProto.QueryPropsRequest.Builder builder = CommonServiceProto.QueryPropsRequest.newBuilder();
+        if (StringUtils.isNotBlank(keyword)) {
+            builder.setKeyword(keyword);
+        }
+
+        CommonServiceProto.CommonResponse response = blockingStub.listProps(builder.build());
+        Map<String, Object> propMap = JSONObject.parseObject(response.getData().toStringUtf8(), Map.class);
+
+        List<Map> list = propMap.entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .map(entry -> {
+                    Map map = Maps.newHashMap();
+                    map.put("key", entry.getKey());
+                    map.put("value", entry.getValue());
+                    return map;
+                })
+                .collect(Collectors.toList());
+
+        Map data = Maps.newHashMap();
+        data.put("total", list.size());
+        data.put("rows", list);
+
+        return ReturnResult.build(response.getStatusCode(), response.getMessage(), data);
     }
 
 
