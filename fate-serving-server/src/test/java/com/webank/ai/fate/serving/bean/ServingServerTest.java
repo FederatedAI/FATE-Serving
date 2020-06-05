5f601aebd32c3d7104cffc9265e0756fb3b2eeca
v_dylanxu <136539068@qq.com>
2020-06-05 11:18:24 +0800

    ok

--- fate-serving-server/src/test/java/com/webank/ai/fate/serving/bean/ServingServerTest.java
+++ fate-serving-server/src/test/java/com/webank/ai/fate/serving/bean/ServingServerTest.java
@@ -1,11 +1,9 @@
 package com.webank.ai.fate.serving.bean;
 
 import com.alibaba.fastjson.JSON;
-import com.google.common.collect.Lists;
 import com.google.protobuf.ByteString;
 import com.webank.ai.fate.api.mlmodel.manager.ModelServiceProto;
 import com.webank.ai.fate.api.serving.InferenceServiceProto;
-import com.webank.ai.fate.serving.core.bean.BatchInferenceRequest;
 import com.webank.ai.fate.serving.core.bean.InferenceRequest;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
@@ -16,7 +14,6 @@ import org.junit.runners.MethodSorters;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.util.List;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
@@ -236,7 +233,7 @@ public class ServingServerTest {
 
     @Test
     public void test_model_load_pb() {
-        URL resource = ModelTest.class.getClassLoader().getResource("host#10000#arbiter-10000#guest-9999#host-10000#model_202006031540378520599");
+        URL resource = ServingServerTest.class.getClassLoader().getResource("host#10000#arbiter-10000#guest-9999#host-10000#model_202006031540378520599");
         String filePath = resource.getPath().replaceAll("%23", "#");
         ModelServiceProto.PublishRequest.Builder publicRequestBuilder = ModelServiceProto.PublishRequest.newBuilder();
         ModelServiceProto.PublishRequest publishRequest = publicRequestBuilder.setLocal(ModelServiceProto.LocalInfo.newBuilder()
