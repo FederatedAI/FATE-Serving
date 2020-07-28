c4d0e5f32c8fda124af95ff793b18bf8a946deb8
utu <jinhuitu@gmail.com>
2020-07-28 16:14:30 +0800

    feature: add TLS for InterGrpcServer.

--- serving-proxy/src/main/java/com/webank/ai/fate/serving/proxy/rpc/grpc/InterGrpcServer.java
+++ serving-proxy/src/main/java/com/webank/ai/fate/serving/proxy/rpc/grpc/InterGrpcServer.java
@@ -3,6 +3,10 @@ import com.webank.ai.fate.register.provider.FateServerBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.ServerInterceptors;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyServerBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
@@ -11,6 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.io.File;
 import java.util.concurrent.Executor;
 
 /**
@@ -22,6 +27,18 @@ public class InterGrpcServer implements InitializingBean {
 
     @Value("${proxy.grpc.inter.port:8869}")
     private Integer port;
+
+    @Value("${proxy.grpc.inter.negotiationType:PLAINTEXT}")
+    private String negotiationType;
+
+    @Value("${proxy.grpc.inter.client.certChain.file:}")
+    private String certChainFilePath;
+
+    @Value("${proxy.grpc.inter.client.privateKey.file:}")
+    private String privateKeyFilePath;
+
+    @Value("${proxy.grpc.inter.CA.file:}")
+    private String trustCertCollectionFilePath;
 
     Logger logger  = LoggerFactory.getLogger(InterGrpcServer.class);
 
@@ -39,7 +56,22 @@ public class InterGrpcServer implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        FateServerBuilder serverBuilder = (FateServerBuilder) ServerBuilder.forPort(port);
+        FateServerBuilder serverBuilder = null;
+        if("TLS".equals(negotiationType)) {
+            if(certChainFilePath.isEmpty() || privateKeyFilePath.isEmpty() || trustCertCollectionFilePath.isEmpty()) {
+                throw new RuntimeException("using TLS, but certificates file paths are missing!");
+            }
+
+            SslContextBuilder sslContextBuilder = SslContextBuilder.forServer(new File(certChainFilePath), new File(privateKeyFilePath));
+            sslContextBuilder.trustManager(new File(trustCertCollectionFilePath));
+            sslContextBuilder.clientAuth(ClientAuth.REQUIRE);
+
+            serverBuilder = new FateServerBuilder(NettyServerBuilder.forPort(port));
+            serverBuilder.sslContext(GrpcSslContexts.configure(sslContextBuilder, SslProvider.OPENSSL).build());
+        } else {
+            serverBuilder = (FateServerBuilder) ServerBuilder.forPort(port);
+        }
+
         serverBuilder.executor(executor);
         serverBuilder.addService(ServerInterceptors.intercept(interRequestHandler, new ServiceExceptionHandler()));
         serverBuilder.addService(interRequestHandler);
