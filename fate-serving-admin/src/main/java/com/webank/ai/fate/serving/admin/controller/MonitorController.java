d16ca3d1d4e0622651685f91e03427895c806f69
ZiyiWang32 <42878011+ZiyiWang32@users.noreply.github.com>
2021-04-22 17:31:39 +0800

    健康检测接口

--- fate-serving-admin/src/main/java/com/webank/ai/fate/serving/admin/controller/MonitorController.java
+++ fate-serving-admin/src/main/java/com/webank/ai/fate/serving/admin/controller/MonitorController.java
@@ -22,7 +22,9 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.webank.ai.fate.api.networking.common.CommonServiceGrpc;
 import com.webank.ai.fate.api.networking.common.CommonServiceProto;
+import com.webank.ai.fate.register.zookeeper.ZookeeperRegistry;
 import com.webank.ai.fate.serving.admin.services.ComponentService;
+import com.webank.ai.fate.serving.common.flow.HealthInfo;
 import com.webank.ai.fate.serving.common.flow.JvmInfo;
 import com.webank.ai.fate.serving.common.flow.MetricNode;
 import com.webank.ai.fate.serving.core.bean.Dict;
@@ -37,10 +39,12 @@ import com.webank.ai.fate.serving.core.utils.NetUtils;
 import io.grpc.ManagedChannel;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -51,9 +55,10 @@ import java.util.stream.Collectors;
 public class MonitorController {
 
     GrpcConnectionPool grpcConnectionPool = GrpcConnectionPool.getPool();
-
     @Autowired
     ComponentService componentService;
+    Map<String,HealthInfo> healthInfoMap;
+
 
     @GetMapping("/monitor/queryJvm")
     public ReturnResult queryJvmData(String host, int port) {
@@ -162,6 +167,28 @@ public class MonitorController {
         return ReturnResult.build(StatusCode.SUCCESS, Dict.SUCCESS, dataMap);
     }
 
+    @GetMapping("monitor/checkHealth")
+    public ReturnResult checkHealthService(String host, int port) {
+        Preconditions.checkArgument(StringUtils.isNotBlank(host), "parameter host is blank");
+        Preconditions.checkArgument(port != 0, "parameter port was wrong");
+
+        if (!NetUtils.isValidAddress(host + ":" + port)) {
+            throw new SysException("invalid address");
+        }
+
+        if (!componentService.isAllowAccess(host, port)) {
+            throw new RemoteRpcException("no allow access, target: " + host + ":" + port);
+        }
+
+        if (healthInfoMap.get(host).getData() == null) {
+            return ReturnResult.build(StatusCode.SYSTEM_ERROR, Dict.FAILED, null);
+        }
+
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put(host,healthInfoMap.get(host + ":" + port).getData());
+        return ReturnResult.build(StatusCode.SUCCESS, Dict.SUCCESS, dataMap);
+    }
+
     private CommonServiceGrpc.CommonServiceBlockingStub getMonitorServiceBlockStub(String host, int port) {
         Preconditions.checkArgument(StringUtils.isNotBlank(host), "parameter host is blank");
         Preconditions.checkArgument(port != 0, "parameter port was wrong");
@@ -179,4 +206,49 @@ public class MonitorController {
         return blockingStub;
     }
 
+    @Scheduled(fixedRate = 10000)
+    private void checkRemoteHealth() {
+        System.out.println("checking health");
+        Map<String,List<String>> addressMap = componentService.getAddressMap();
+        if (addressMap == null) {
+            return;
+        }
+        for(String component: addressMap.keySet()) {
+            if (component.equals("admin")) {
+                continue;
+            }
+            for(String address: addressMap.get(component)) {
+                String host = address.substring(0,address.indexOf(":"));
+                int port = Integer.parseInt(address.substring(address.indexOf(":") + 1));
+                CommonServiceGrpc.CommonServiceBlockingStub blockingStub = getMonitorServiceBlockStub(host, port);
+                CommonServiceProto.HealthCheckRequest.Builder builder = CommonServiceProto.HealthCheckRequest.newBuilder();
+                builder.setMode("Test");
+                builder.setVersion("0.1");
+                builder.setType("Test");
+                CommonServiceProto.CommonResponse commonResponse = blockingStub.checkHealthService(builder.build());
+                if (healthInfoMap == null) {
+                    healthInfoMap = new HashMap<>();
+                }
+                if (healthInfoMap.containsKey(address)) {
+                    HealthInfo currentHostInfo = healthInfoMap.get(address);
+                    if (commonResponse.getData() == null || commonResponse.getData().toStringUtf8() == "null") {
+                        currentHostInfo.setData(null);
+                    }
+                    else{
+                        currentHostInfo.setData(commonResponse.getData());
+                    }
+                }
+                else {
+                    HealthInfo newHealthInfo = new HealthInfo(component,host,port);
+                    if (commonResponse.getData() == null || commonResponse.getData().toStringUtf8() == "null") {
+                        newHealthInfo.setData(null);
+                    }
+                    else{
+                        newHealthInfo.setData(commonResponse.getData());
+                    }
+                    healthInfoMap.put(address,newHealthInfo);
+                }
+            }
+        }
+    }
 }
---
+++ fate-serving-common/src/main/java/com/webank/ai/fate/serving/common/flow/HealthInfo.java
@@ -0,0 +1,2 @@
+package com.webank.ai.fate.serving.common.flow;public class healthInfo {
+}
