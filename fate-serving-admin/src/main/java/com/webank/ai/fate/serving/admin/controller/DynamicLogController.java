91d2008d71d020372384a84e8516cb58f4eb6c9a
hecy7 <hecy7@asiainfo.com>
2023-09-05 14:44:51 +0800

    Add dynamic adjustment log interface To Admin Serving Proxy
    Signed-off-by: hecy7 <hecy7@asiainfo.com>

---
+++ fate-serving-admin/src/main/java/com/webank/ai/fate/serving/admin/controller/DynamicLogController.java
@@ -0,0 +1,41 @@
+package com.webank.ai.fate.serving.admin.controller;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author hcy
+ */
+@RequestMapping("/admin")
+@RestController
+public class DynamicLogController {
+    private static final Logger logger = LoggerFactory.getLogger(DynamicLogController.class);
+
+    @GetMapping("/alterSysLogLevel/{level}")
+    public String alterSysLogLevel(@PathVariable String level){
+        try {
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+            context.getLogger("ROOT").setLevel(Level.valueOf(level));
+            return "ok";
+        } catch (Exception ex) {
+            logger.error("admin alterSysLogLevel failed : " + ex);
+            return "failed";
+        }
+
+    }
+
+    @GetMapping("/alterPkgLogLevel")
+    public String alterPkgLogLevel(@RequestParam String level, @RequestParam String pkgName){
+        try {
+            LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+            context.getLogger(pkgName).setLevel(Level.valueOf(level));
+            return "ok";
+        } catch (Exception ex) {
+            logger.error("admin alterPkgLogLevel failed : " + ex);
+            return "failed";
+        }
+    }
+}
