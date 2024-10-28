68ac9e3cf40a4e97026e7143d47398982100c857
hecy7 <hecy7@asiainfo.com>
2024-10-28 15:42:57 +0800

    优化Disruptor消息库的使用以达到生产级流量下更好的性能
    Signed-off-by: hecy7 <hecy7@asiainfo.com>

--- fate-serving-common/src/main/java/com/webank/ai/fate/serving/common/async/DisruptorExceptionHandler.java
+++ fate-serving-common/src/main/java/com/webank/ai/fate/serving/common/async/DisruptorExceptionHandler.java
@@ -22,23 +22,20 @@ import org.slf4j.LoggerFactory;
 
 public class DisruptorExceptionHandler implements ExceptionHandler {
 
-    private static Logger logger = LoggerFactory.getLogger(DisruptorExceptionHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(DisruptorExceptionHandler.class);
 
     @Override
     public void handleEventException(Throwable ex, long sequence, Object event) {
-        logger.error("Disruptor event exception, {}", ex.getMessage());
-        ex.printStackTrace();
+        logger.error("Disruptor event exception, {}", ex.getMessage(), ex);
     }
 
     @Override
     public void handleOnStartException(Throwable ex) {
-        logger.error("Disruptor start exception, {}", ex.getMessage());
-        ex.printStackTrace();
+        logger.error("Disruptor start exception, {}", ex.getMessage(), ex);
     }
 
     @Override
     public void handleOnShutdownException(Throwable ex) {
-        logger.error("Disruptor shutdown exception, {}", ex.getMessage());
-        ex.printStackTrace();
+        logger.error("Disruptor shutdown exception, {}", ex.getMessage(), ex);
     }
 }
