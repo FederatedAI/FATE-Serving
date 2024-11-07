c143a8caaa933ce094f6c036d5fbd8feba9d6023
hecy7 <hecy7@asiainfo.com>
2024-11-07 13:07:40 +0800

    增加Auth文件定时加载周期降低CPU消耗；单元素生成list采用singleton类型list；去除无效boxing操作
    Signed-off-by: hecy7 <hecy7@asiainfo.com>

--- fate-serving-common/src/main/java/com/webank/ai/fate/serving/common/rpc/core/Interceptor.java
+++ fate-serving-common/src/main/java/com/webank/ai/fate/serving/common/rpc/core/Interceptor.java
@@ -20,11 +20,11 @@ import com.webank.ai.fate.serving.core.bean.Context;
 
 public interface Interceptor<req, resp> {
 
-    default public void doPreProcess(Context context, InboundPackage<req> inboundPackage, OutboundPackage<resp> outboundPackage) throws Exception {
+    default void doPreProcess(Context context, InboundPackage<req> inboundPackage, OutboundPackage<resp> outboundPackage) throws Exception {
 
     }
 
-    default public void doPostProcess(Context context, InboundPackage<req> inboundPackage, OutboundPackage<resp> outboundPackage) throws Exception {
+    default void doPostProcess(Context context, InboundPackage<req> inboundPackage, OutboundPackage<resp> outboundPackage) throws Exception {
 
     }
 
